199d76275f0d5f11817272d33d24772f65232035
th0tminik <49837535+th0tminik@users.noreply.github.com>
2022-05-11 19:08:39 +0200

    Igrac se sad okrece kako se i kamera okrece

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,11 +11,16 @@ public class PlayerController : MonoBehaviour
 
     public float xRange = 10;
 
+    public GameObject followTarget;
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
+
+
+    private float smooth = 0f;
 
     // Update is called once per frame
     void Update()
@@ -28,5 +33,13 @@ public class PlayerController : MonoBehaviour
 
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
+
+        // rotates the player towards where camera is aiming
+
+        Quaternion target = Quaternion.Euler(0, followTarget.transform.localRotation.eulerAngles.y, 0);
+
+        // Dampen towards the target rotation
+
+         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
     }
 }
