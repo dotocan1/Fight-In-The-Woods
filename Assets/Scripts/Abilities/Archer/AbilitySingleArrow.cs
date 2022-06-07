97e71db1e7c89f28f2e38be46a9d58587f100ac9
th0tminik <49837535+dotocan1@users.noreply.github.com>
2022-06-07 13:59:47 +0200

    Ubacen cooldown na single arrow

--- Assets/Scripts/Abilities/Archer/AbilitySingleArrow.cs
+++ Assets/Scripts/Abilities/Archer/AbilitySingleArrow.cs
@@ -8,13 +8,35 @@ public class AbilitySingleArrow : MonoBehaviour
     private GameObject instantiatedObj;
     public Camera fpsCam;
 
+    public bool IsAvailable = true;
+    public float CooldownDuration = 10.0f;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            // if not available to use (still cooling down) just exit
+            if (IsAvailable == false)
+            {
+                return;
+            }
+
+            // made it here then ability is available to use...
+            // UseAbilityCode goes here
+
             instantiatedObj = (GameObject)Instantiate(projectilePrefab, transform.position + (transform.forward) + (transform.up), fpsCam.transform.rotation);
             Destroy(instantiatedObj, 10f);
+
+            // start the cooldown timer
+            StartCoroutine(StartCooldown());
         }
+    }
+
+    public IEnumerator StartCooldown()
+    {
+        IsAvailable = false;
+        yield return new WaitForSeconds(CooldownDuration);
+        IsAvailable = true;
     }
 }
