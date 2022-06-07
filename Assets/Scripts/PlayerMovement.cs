d53f739767492ce7ebe1197786382f6edcc6a1ba
th0tminik <49837535+dotocan1@users.noreply.github.com>
2022-06-07 17:34:33 +0200

    Animacija za trcanje sada funkcionira, ali kamera se mice kako karakter trci

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,9 @@ public class PlayerMovement : MonoBehaviour
     static readonly int jumpTrigger = Animator.StringToHash("Jump");
     static readonly int jumpState = Animator.StringToHash("Jump");
 
-    private Animator anim;
+    private Animator animator;
 
-  
+
     private bool sprinting;
     private bool jump;
     public float speed = 12f;
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     private float smooth = 0.5f;
@@ -43,17 +43,20 @@ public class PlayerMovement : MonoBehaviour
         //sprinting = Input.GetButton("Sprint");
         //jump = Input.GetButtonDown("Jump");
 
-        if (jump && 
-            anim.GetCurrentAnimatorStateInfo(0).shortNameHash != jumpState && 
-            anim.GetNextAnimatorStateInfo(0).shortNameHash != jumpState) 
-                anim.SetTrigger(jumpTrigger);
+        if (jump &&
+            animator.GetCurrentAnimatorStateInfo(0).shortNameHash != jumpState &&
+            animator.GetNextAnimatorStateInfo(0).shortNameHash != jumpState)
+            animator.SetTrigger(jumpTrigger);
 
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    if (sprinting) move *= 2;
-   
-        //    //anim.SetFloat(forwardFloat, verticalInput, 0.1f, Time.deltaTime);
-        //}
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("isRunning", true);
+        }
+
+        if (!Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("isRunning", false);
+        }
 
         // sprinting
 
