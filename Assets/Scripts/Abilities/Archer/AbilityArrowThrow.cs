eb66b80e2ad74300d9ee762873ade54b2ab392b1
th0tminik <49837535+dotocan1@users.noreply.github.com>
2022-06-07 14:58:53 +0200

    Arrow Circle gotov (placement malo cudan, trebalo bi popraviti))

--- Assets/Scripts/Abilities/Archer/AbilityArrowThrow.cs
+++ Assets/Scripts/Abilities/Archer/AbilityArrowThrow.cs
@@ -8,13 +8,37 @@ public class AbilityArrowThrow : MonoBehaviour
     private GameObject instantiatedObj;
     public Camera fpsCam;
 
+    public bool IsAvailable = true;
+    public float CooldownDuration = 10.0f;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            instantiatedObj = (GameObject)Instantiate(projectilePrefab, transform.position + (transform.forward) + (transform.up), fpsCam.transform.rotation);
+
+            // if not available to use (still cooling down) just exit
+            if (IsAvailable == false)
+            {
+                return;
+            }
+
+            // made it here then ability is available to use...
+            // UseAbilityCode goes here
+
+            instantiatedObj = (GameObject)Instantiate(projectilePrefab, transform.position, transform.rotation);
             Destroy(instantiatedObj, 10f);
+
+            // start the cooldown timer
+            StartCoroutine(StartCooldown());
         }
+
+    }
+
+    public IEnumerator StartCooldown()
+    {
+        IsAvailable = false;
+        yield return new WaitForSeconds(CooldownDuration);
+        IsAvailable = true;
     }
 }
