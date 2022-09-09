2762cb92d50b91a0b24372f6a5e2380e1019c940
th0tminik <49837535+dotocan1@users.noreply.github.com>
2022-09-09 22:32:21 +0200

    Update AbilityWaterThrow.cs

--- Assets/Scripts/Abilities/BadMage/AbilityWaterThrow.cs
+++ Assets/Scripts/Abilities/BadMage/AbilityWaterThrow.cs
@@ -2,22 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using System;
-using System.Timers;
 
 public class AbilityWaterThrow : MonoBehaviour
 {
 
     private GameObject instantiatedObj;
     public Camera fpsCam;
-
-    public bool IsAvailable = true;
-
     PhotonView view;
-
-    private float cooldownDuration = 8f;
-    [SerializeField]
-    private float fireDelay = 20f;
 
     private void Start()
     {
@@ -31,7 +22,7 @@ public class AbilityWaterThrow : MonoBehaviour
             Debug.Log("Uspjesno unistena skripta");
         }
     }
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Q))
