7e9652ade85a4fd9450a6e79abf7f11eff66d039
th0tminik <49837535+dotocan1@users.noreply.github.com>
2022-09-15 17:05:10 +0200

    izbrisan cooldown

--- Assets/Scripts/Abilities/BadMage/AbilityWaterThrow.cs
+++ Assets/Scripts/Abilities/BadMage/AbilityWaterThrow.cs
@@ -10,12 +10,6 @@ public class AbilityWaterThrow : MonoBehaviour
     public Camera fpsCam;
     PhotonView view;
 
-    [SerializeField]public float fireRate = 100f;
-
-    private float nextFire;
-
-
-
     private void Start()
     {
         view = GetComponent<PhotonView>();
@@ -23,7 +17,6 @@ public class AbilityWaterThrow : MonoBehaviour
 
         if (!view.IsMine)
         {
-
             abilityWaterThrow.enabled = false;
             Debug.Log("Uspjesno unistena skripta");
         }
@@ -33,19 +26,9 @@ public class AbilityWaterThrow : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if(!(nextFire + fireRate < Time.time))
-            {
-                Debug.Log("this is not firing");
-                return;
-            }
-            
-            nextFire = Time.time + fireRate;
 
             instantiatedObj = PhotonNetwork.Instantiate("Abilities/BadMage/WaterThrow", transform.position + (transform.forward * 1) + (transform.up * 1.5f), fpsCam.transform.rotation);
             Destroy(instantiatedObj, 0.5f);
-
-           
-
         }
     }
 }
