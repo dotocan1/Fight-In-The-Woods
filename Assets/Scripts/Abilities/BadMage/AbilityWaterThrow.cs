bff81a07f1a488f8edb1fa8bbe87fec9b5650521
Dominik Otočan <dominik.otocan1@gmail.com>
2022-09-16 10:50:10 +0200

    Cooldown sada funkcionira

--- Assets/Scripts/Abilities/BadMage/AbilityWaterThrow.cs
+++ Assets/Scripts/Abilities/BadMage/AbilityWaterThrow.cs
@@ -12,7 +12,9 @@ public class AbilityWaterThrow : MonoBehaviour
 
     // timer
 
-    float gametimer = 0f;
+    private float gametimer = 0f;
+    // private float coolDownTime;
+    private float abilityUsed;
 
 
     private void Start()
@@ -30,10 +32,14 @@ public class AbilityWaterThrow : MonoBehaviour
     void Update()
     {
 
+        gametimer += Time.deltaTime;
+        int seconds = (int)(gametimer % 60);
+        int minutes = (int)(gametimer / 60);
 
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-
+        // zadnji broj u uvjetu oznacava vrijeme cooldowna
+        if (Input.GetKeyDown(KeyCode.Q) && (gametimer - abilityUsed) > 5.0f)
+        {   
+            abilityUsed = gametimer;
             instantiatedObj = PhotonNetwork.Instantiate("Abilities/BadMage/WaterThrow", transform.position + (transform.forward * 1) + (transform.up * 1.5f), fpsCam.transform.rotation);
             Destroy(instantiatedObj, 0.5f);
         }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,7 +8,6 @@ public class Timer : MonoBehaviour
     public Text gameTimerText;
     float gametimer = 0f;
 
-
     // Start is called before the first frame update
     void Start()
     {
